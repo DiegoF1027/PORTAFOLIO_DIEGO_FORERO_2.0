2b7947a425e7fe5d4805c3d6bf988d1f42760279
Diego Forero <119710086+DiegoF1027@users.noreply.github.com>
2025-07-05 16:47:42 -0500

    Creación de la sección de proyectos destacados - actualmente hay un ejemplo del renderizado

---
+++ js/proyectosPortfolio.js
@@ -0,0 +1,13 @@
+// Datos de los proyectos del portfolio
+const proyectos = [
+    {
+        titulo: "Landing promocional para serie",
+        descripcion: "Sitio promocional responsive para la serie Adventure Time, con integración de newsletter y características dinámicas que mantienen a los fans actualizados con las últimas aventuras.",
+        imagen: "./img/adventure-time.png",
+        año: 2023,
+        rol: "Desarrollador Frontend",
+        categoria: "Trabajo Conceptual",
+        demoUrl: "#",
+        githubUrl: "#"
+    }
+];
